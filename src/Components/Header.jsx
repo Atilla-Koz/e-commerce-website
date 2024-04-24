9c152d554c9609a8d6b33720de4303fdf83299da
Atilla-Koz <atillakoz@hotmail.com>
2024-04-24 14:16:36 +0300

    Refactor Header component and remove unused assets

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,38 @@
+import phone from '../assets/Header/phone.svg';
+import mail from '../assets/Header/mail.svg';
+import instagram from '../assets/Header/insta.svg';
+import youtube from '../assets/Header/youtube.svg';
+import facebook from '../assets/Header/facebook.svg';
+import twitter from '../assets/Header/twitter.svg';
+
 export default function Header() {
   return (
-    <header className="header">
-      <h1 className="header__title">Our store</h1>
-      <nav className="header__nav">
-        <a href="/" className="header__link">
-          Home
-        </a>
-        <a href="/products" className="header__link">
-          Products
-        </a>
-      </nav>
+    <header className="flex flex-col">
+      <span className="hidden lg:block">
+        <section className="flex flex-row items-center justify-between text-white bg-headerTop w-full h-14 px-6">
+          <div className="flex flex-row gap-6">
+            <p className="flex flex-row  gap-1">
+              <img src={phone} alt="Phone icon" />
+              (225) 555-0118
+            </p>
+            <p className="flex flex-row  gap-1">
+              <img src={mail} alt="Phone icon" />
+              michelle.rivera@example.com
+            </p>
+          </div>
+          <div>
+            <p>Follow Us and get a chance to win 80% off</p>
+          </div>
+          <div className="flex flex-row gap-2">
+            <p>Follow Us:</p>
+            <img src={instagram} alt="Instagram icon" />
+            <img src={youtube} alt="Youtube icon" />
+            <img src={facebook} alt="Facebook icon" />
+            <img src={twitter} alt="Twitter icon" />
+          </div>
+        </section>
+      </span>
+      <section className="flex xl:flex-row flex-col">dasdasd</section>
     </header>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
-// tailwind.config.js
-
 module.exports = {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        headerTop: '#252B42',
+        white: '#FFFFFF',
+      },
+    },
   },
   plugins: [],
 };
