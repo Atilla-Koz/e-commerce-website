21e635734a45d2590e5872cf86b1c6d391f9a9dc
Atilla-Koz <atillakoz@hotmail.com>
2024-04-25 16:38:06 +0300

    Refactor Hero component and update styles in Hero.jsx and App.css

--- src/Components/HomePage/Hero.jsx
+++ src/Components/HomePage/Hero.jsx
@@ -7,9 +7,9 @@ export default function Hero() {
   return (
     <div className="pt-[1rem]">
       <section className="flex justify-center items-center ">
-        <div className="w-11/12 h-[622px] ">
+        <div className="w-11/12 h-[722px] ">
           <Swiper className="">
-            <SwiperSlide className="flex lg:flex-row flex-col  lg:items-center  rounded-3xl bg-gradient-to-r from-blue-200 to-teal-200 hover:from-teal-200 hover:to-blue-200 transition duration-500 ease-in-out">
+            <SwiperSlide className="flex lg:flex-row flex-col  lg:items-center rounded-3xl bg-gradient-to-r from-blue-200 to-teal-200 hover:from-teal-200 hover:to-blue-200 transition duration-500 ease-in-out">
               <div className="flex flex-1 flex-col lg:items-start items-center ">
                 <h5 className="text-[#2A7CC7]">SUMMER 2024</h5>
                 <h1 className="pb-[1rem]">NEW COLLECTION</h1>
@@ -27,8 +27,23 @@ export default function Hero() {
                 />
               </div>
             </SwiperSlide>
-            <SwiperSlide className="bg-gradient-to-r from-blue-200 to-teal-200 hover:from-teal-200 hover:to-blue-200 transition duration-500 ease-in-out flex items-center justify-center">
-              2
+            <SwiperSlide className="flex lg:flex-row flex-col  lg:items-center rounded-3xl bg-gradient-to-r from-blue-200 to-teal-200 hover:from-teal-200 hover:to-blue-200 transition duration-500 ease-in-out">
+              <div className="flex flex-1 flex-col lg:items-start items-center ">
+                <h5 className="text-[#2A7CC7]">SUMMER 2024</h5>
+                <h1 className="pb-[1rem]">NEW COLLECTION</h1>
+                <h4>We know how large objects will act, </h4>
+                <h4 className="pb-6">but things on a small scale.</h4>
+                <button className="p-[15px 40px 15px 40px] bg-[#23A6F0] ]">
+                  <h3 className="text-white">SHOP NOW</h3>
+                </button>
+              </div>
+              <div className="flex-1 ">
+                <img
+                  className="lg:absolute bottom-1 right-1 "
+                  src={gin}
+                  alt="ginger"
+                />
+              </div>
             </SwiperSlide>
           </Swiper>
         </div>
--- src/Components/HomePage/Pick.jsx
+++ src/Components/HomePage/Pick.jsx
@@ -1,8 +1,19 @@
+import i1 from '../../assets/Pick/1.svg';
+import i2 from '../../assets/Pick/2.svg';
+import i3 from '../../assets/Pick/3.svg';
+import i4 from '../../assets/Pick/4.svg';
+import i5 from '../../assets/Pick/5.svg';
+import i6 from '../../assets/Pick/6.svg';
+
 export default function Pick() {
   return (
-    <div className="pick">
-      <h2 className="pick__title">Pick your favorite</h2>
-      <div className="pick__list"></div>
+    <div className="flex lg:flex-row flex-col justify-around items-center gap-8 pt-24 pb-24">
+      <img className=" max-w-[50%]" src={i1} alt="1" />
+      <img className=" max-w-[50%]" src={i2} alt="2" />
+      <img className=" max-w-[50%]" src={i3} alt="3" />
+      <img className=" max-w-[50%]" src={i4} alt="4" />
+      <img className=" max-w-[50%]" src={i5} alt="5" />
+      <img className=" max-w-[50%]" src={i6} alt="6" />
     </div>
   );
 }
