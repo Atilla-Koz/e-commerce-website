f8b1fa51152931d921ca5e1d64b09f8a862f9fe2
Atilla-Koz <atillakoz@hotmail.com>
2024-04-29 16:41:45 +0300

    Add product images and update styles in Products.jsx

--- src/Components/HomePage/Products.jsx
+++ src/Components/HomePage/Products.jsx
@@ -1,8 +1,43 @@
+import dc from '../../assets/Products/dc.png';
+import ginger from '../../assets/Products/ginger.jpg';
+import b from '../../assets/Products/b.jpg';
+
 export default function Products() {
   return (
-    <div className="products">
-      <h2 className="products__title">Our products</h2>
-      <div className="products__list"></div>
-    </div>
+    <section className="flex flex-col items-center gap-5">
+      <div
+        className="w-[354px] h-[556px] flex items-end bg-cover bg-center"
+        style={{ backgroundImage: `url(${dc})` }}
+      >
+        <div className="flex flex-col pl-4 justify-evenly w-full h-[238px] bg-[#2D8BC0BF]">
+          <h3 className="text-white">Top Product Of the Week</h3>
+          <button className="w-[198]  h-[52px] p-[15px 40px 15px 40px] rounded-md text-white border border-dashed border-white">
+            <span className="font-[700] text-[14px]"> EXPLORE ITEMS</span>
+          </button>
+        </div>
+      </div>
+      <div
+        className="w-[354px] h-[556px]  flex items-end bg-cover bg-center"
+        style={{ backgroundImage: `url(${ginger})` }}
+      >
+        <div className="flex flex-col pl-4 justify-evenly w-full h-[238px] bg-[#2D8BC0BF]">
+          <h3 className="text-white">Top Product Of the Week</h3>
+          <button className="w-[198]  h-[52px] p-[15px 40px 15px 40px] rounded-md text-white border border-dashed border-white">
+            <span className="font-[700] text-[14px]"> EXPLORE ITEMS</span>
+          </button>
+        </div>
+      </div>
+      <div
+        className="w-[354px] h-[556px] flex items-end bg-cover bg-center"
+        style={{ backgroundImage: `url(${b})` }}
+      >
+        <div className="flex flex-col pl-4 justify-evenly w-full h-[238px] bg-[#2D8BC0BF]">
+          <h3 className="text-white">Top Product Of the Week</h3>
+          <button className="w-[198]  h-[52px] p-[15px 40px 15px 40px] rounded-md text-white border border-dashed border-white">
+            <span className="font-[700] text-[14px]"> EXPLORE ITEMS</span>
+          </button>
+        </div>
+      </div>
+    </section>
   );
 }
