1588ea6edb96325494b38534263f8bbcefdcc1be
Atilla-Koz <atillakoz@hotmail.com>
2024-04-30 10:26:52 +0300

    Update Services component with new layout and styles

--- src/Components/HomePage/Services.jsx
+++ src/Components/HomePage/Services.jsx
@@ -1,8 +1,26 @@
+import white from '../../assets/OurServices/white.png';
+import orange from '../../assets/OurServices/orange.png';
+
 export default function Services() {
   return (
-    <div className="services">
-      <h2 className="services__title">Our services</h2>
-      <div className="services__list"></div>
-    </div>
+    <section className="flex flex-col items-center gap-8 w-[414px] h-[910px] pt-48">
+      <div className="w-[280px] h-[336px] flex flex-col gap-4">
+        <h5 className="text-click">Featured Products</h5>
+        <h2 className="text-title">We love what we do</h2>
+        <p className="text-softText">
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
+        <p className="text-softText pt-8">
+          {' '}
+          Problems trying to resolve the conflict between the two major realms
+          of Classical physics: Newtonian mechanics
+        </p>
+      </div>
+      <div className="flex flex-row justify-center gap-2  ">
+        <img src={white} alt="" />
+        <img src={orange} alt="" />
+      </div>
+    </section>
   );
 }
