e8346dba3031be9fcc41c595a7c3c8d1962e4b99
Atilla-Koz <atillakoz@hotmail.com>
2024-05-02 16:13:20 +0300

    Update layout and styles in Services and FeaturedPosts components

--- src/Components/HomePage/FeaturedPosts.jsx
+++ src/Components/HomePage/FeaturedPosts.jsx
@@ -1,11 +1,10 @@
 import one from '../../assets/Featured/1.png';
 import two from '../../assets/Featured/2.png';
-import three from '../../assets/Featured/3.png';
 import four from '../../assets/Featured/4.png';
 
 export default function FeaturedPosts() {
   return (
-    <section className="flex flex-col items-center gap-32 pt-24 w-[414px] h-[1214px]">
+    <section className="flex flex-col items-center gap-32 pt-24 w-full h-[1214px] lg:w-full lg:h-[632px]">
       <div className="flex flex-col items-center gap-2 w-[248px] h-[122px]">
         <h4 className="text-softText">Featured Products</h4>
         <h3 className="text-title">THE BEST SERVICES</h3>
@@ -13,26 +12,22 @@ export default function FeaturedPosts() {
           Problems trying to resolve the conflict between{' '}
         </p>
       </div>
-      <div className="flex flex-col items-center gap-4 ">
-        <img src={one} />
-
-        <h3 className="text-title">Easy Wins</h3>
-        <p className="text-softText ">Get your best looking smile now!</p>
-      </div>
-      <div className="flex flex-col items-center gap-4 x">
-        <img src={two} />
-        <h3 className="text-title ]">Concrete</h3>
-        <p className="text-softText w-[207px]">
-          Defalcate is most focused in helping you discover your most beautiful
-          smile
-        </p>
-      </div>
-      <div className="flex flex-col items-center gap-4">
-        <img src={four} />
-        <h3 className="text-title"> Hack Growth</h3>
-        <p className="text-softText">
-          Overcame any hurdle or any other problem.
-        </p>
+      <div className="lg:flex lg:flex-row lg:justify-around flex flex-col gap-32 ">
+        <div className="flex flex-col justify-between  items-center gap-4  ">
+          <img src={one} />
+          <h3 className="text-title">Easy Wins</h3>
+          <p className="text-softText ">Get your best looking smile now!</p>
+        </div>
+        <div className="flex flex-col justify-between items-center gap-4 x">
+          <img src={two} />
+          <h3 className="text-title ]">Concrete</h3>
+          <p className="text-softText  ">Get your best looking smile now!</p>
+        </div>
+        <div className="flex flex-col justify-between items-center gap-4">
+          <img src={four} />
+          <h3 className="text-title"> Hack Growth</h3>
+          <p className="text-softText">Get your best looking smile now!</p>
+        </div>
       </div>
     </section>
   );
--- src/Components/HomePage/Services.jsx
+++ src/Components/HomePage/Services.jsx
@@ -3,7 +3,7 @@ import orange from '../../assets/OurServices/orange.png';
 
 export default function Services() {
   return (
-    <section className="flex flex-col items-center gap-8 w-[414px] h-[910px] pt-48 lg:flex lg:flex-row-reverse lg:justify-center lg:gap-24 lg:w-full lg:h-[658px] ">
+    <section className="flex flex-col items-center gap-8 w-full h-[910px] pt-48 lg:flex lg:flex-row-reverse lg:justify-center lg:gap-24 lg:w-full lg:h-[658px] ">
       <div className="w-[280px] h-[336px] flex flex-col gap-4 lg:w-[447px] lg:h-[246px] ">
         <h5 className="text-click">Featured Products</h5>
         <h2 className="text-title">We love what we do</h2>
