6e31a1101bb962c4b80d25c2e8ba67cfd1ab2c00
Atilla-Koz <atillakoz@hotmail.com>
2024-05-03 14:04:27 +0300

    Add ShopPage component and route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css';
 import { Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 import HomePage from './Components/HomePage/HomePage';
+import ShopPage from './Components/ShopPage';
 
 function App() {
   return (
     <>
       <Switch>
+        <Route path="/shop">
+          <ShopPage />
+        </Route>
         <Route path="/">
           <HomePage />
         </Route>
---
+++ src/Components/ShopPage.jsx
@@ -0,0 +1,12 @@
+import Header from './Header';
+import Footer from './Footer';
+import ShopNav from './ShopPage/ShopNav';
+export default function ShopPage() {
+  return (
+    <div>
+      <Header />
+      <ShopNav />
+      <Footer />
+    </div>
+  );
+}
