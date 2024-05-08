21849ba81c6ec6c7cb642d9df9227643239ee439
Atilla-Koz <atillakoz@hotmail.com>
2024-05-08 10:03:32 +0300

    feat: Add lazy loading for ShopHero images

--- src/Components/ShopPage/ShopHero.jsx
+++ src/Components/ShopPage/ShopHero.jsx
@@ -1,7 +1,9 @@
+import one from '../../assets/ShopPage/ShopNav/ShopHero/1.png';
+import two from '../../assets/ShopPage/ShopNav/ShopHero/2.png';
 export default function ShopHero() {
   return (
-    <div>
-      <h1>ShopHero</h1>
-    </div>
+    <search className="w-full h-[1628px] flex flex-col items-center">
+      <img src={two} />
+    </search>
   );
 }
