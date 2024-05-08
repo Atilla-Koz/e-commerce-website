1170c1140c37a6012cd9353c412f0d6db4babc5c
Atilla-Koz <atillakoz@hotmail.com>
2024-05-08 10:08:23 +0300

    refactor: Update ShopHero component with lazy loading for images

--- src/Components/ShopPage/ShopHero.jsx
+++ src/Components/ShopPage/ShopHero.jsx
@@ -1,9 +1,14 @@
-import one from '../../assets/ShopPage/ShopNav/ShopHero/1.png';
 import two from '../../assets/ShopPage/ShopNav/ShopHero/2.png';
+import three from '../../assets/ShopPage/ShopNav/ShopHero/3.png';
+
 export default function ShopHero() {
   return (
-    <search className="w-full h-[1628px] flex flex-col items-center">
+    <search className="w-full h-[1628px] flex flex-col items-center gap-4">
+      <img src={three} />
       <img src={two} />
+      <img src={three} />
+      <img src={two} />
+      <img src={three} />
     </search>
   );
 }
