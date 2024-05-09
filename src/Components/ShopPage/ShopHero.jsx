bdf79010295fe98681c0f3ad5a8f3fb94a9b1cf8
Atilla-Koz <atillakoz@hotmail.com>
2024-05-09 10:56:26 +0300

    chore: Update ShopHero component with responsive layout and lazy loading for images

--- src/Components/ShopPage/ShopHero.jsx
+++ src/Components/ShopPage/ShopHero.jsx
@@ -3,12 +3,12 @@ import three from '../../assets/ShopPage/ShopNav/ShopHero/3.png';
 
 export default function ShopHero() {
   return (
-    <search className="w-full h-[1628px] flex flex-col items-center gap-4">
-      <img src={three} />
-      <img src={two} />
-      <img src={three} />
-      <img src={two} />
-      <img src={three} />
+    <search className="w-full h-[1628px] flex flex-col items-center justify-center gap-4 bg-[#FAFAFA] lg:flex-row lg:h-[271px] lg:pb-[3rem]">
+      <img className="lg:w-[204] lg:h-[223px]" src={three} />
+      <img className="lg:w-[204] lg:h-[223px]" src={two} />
+      <img className="lg:w-[204] lg:h-[223px]" src={three} />
+      <img className="lg:w-[204] lg:h-[223px]" src={two} />
+      <img className="lg:w-[204] lg:h-[223px]" src={three} />
     </search>
   );
 }
