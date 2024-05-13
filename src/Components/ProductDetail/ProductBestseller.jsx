d5ab20d926baa2d450616acc649ab61cb6f1e4cb
Atilla-Koz <atillakoz@hotmail.com>
2024-05-13 13:41:33 +0300

    refactor: Improve ProductBestseller component with responsive layout and lazy loading for images

--- src/Components/ProductDetail/ProductBestseller.jsx
+++ src/Components/ProductDetail/ProductBestseller.jsx
@@ -5,60 +5,103 @@ export default function ProductBestseller() {
   return (
     <section className="flex flex-col items-center gap-8  ">
       <h3>BESTSELLER PRODUCTS</h3>
-      <div className="">
-        <img src={catal} />
-      </div>
-      <div className="flex flex-col items-start gap-4 w-[75%]">
-        <h5>Graphic Design</h5>
-        <Link>English Department</Link>
-        <span className="flex flex-row gap-4">
-          <h5 className="text-softText">$16.48</h5>
-          <h5 className="text-newP">$6.48</h5>
-        </span>
-      </div>
-      <div className="">
-        <img src={catal} />
-      </div>
-      <div className="flex flex-col items-start gap-4 w-[75%]">
-        <h5>Graphic Design</h5>
-        <Link>English Department</Link>
-        <span className="flex flex-row gap-4">
-          <h5 className="text-softText">$16.48</h5>
-          <h5 className="text-newP">$6.48</h5>
-        </span>
-      </div>
-      <div className="">
-        <img src={catal} />
-      </div>
-      <div className="flex flex-col items-start gap-4 w-[75%]">
-        <h5>Graphic Design</h5>
-        <Link>English Department</Link>
-        <span className="flex flex-row gap-4">
-          <h5 className="text-softText">$16.48</h5>
-          <h5 className="text-newP">$6.48</h5>
-        </span>
-      </div>
-      <div className="">
-        <img src={catal} />
-      </div>
-      <div className="flex flex-col items-start gap-4 w-[75%]">
-        <h5>Graphic Design</h5>
-        <Link>English Department</Link>
-        <span className="flex flex-row gap-4">
-          <h5 className="text-softText">$16.48</h5>
-          <h5 className="text-newP">$6.48</h5>
-        </span>
-      </div>
-      <div className="">
-        <img src={catal} />
-      </div>
-      <div className="flex flex-col items-start gap-4 w-[75%]">
-        <h5>Graphic Design</h5>
-        <Link>English Department</Link>
-        <span className="flex flex-row gap-4">
-          <h5 className="text-softText">$16.48</h5>
-          <h5 className="text-newP">$6.48</h5>
-        </span>
+      <div className="lg:flex lg:flex-row lg:flex-wrap lg:justify-center gap-8">
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>{' '}
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
+        <div className="">
+          <img src={catal} />
+
+          <div className="flex flex-col items-start gap-4 w-[75%]">
+            <h5>Graphic Design</h5>
+            <Link>English Department</Link>
+            <span className="flex flex-row gap-4">
+              <h5 className="text-softText">$16.48</h5>
+              <h5 className="text-newP">$6.48</h5>
+            </span>
+          </div>
+        </div>
       </div>
     </section>
   );
