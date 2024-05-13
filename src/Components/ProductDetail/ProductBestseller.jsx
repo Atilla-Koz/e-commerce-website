76bbddd99ce6524c4cca13fc933dd49b9f80c8eb
Atilla-Koz <atillakoz@hotmail.com>
2024-05-13 13:50:19 +0300

    refactor: Add react-router-dom Link to ShopProductCards for navigation to product details page

--- src/Components/ProductDetail/ProductBestseller.jsx
+++ src/Components/ProductDetail/ProductBestseller.jsx
@@ -2,106 +2,77 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min';
 import catal from '../../assets/ProductDetail/catal.png';
 
 export default function ProductBestseller() {
+  // Ürün verilerini bir dizi içinde saklayalım
+  const products = [
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+    {
+      name: 'Graphic Design',
+      department: 'English Department',
+      price: '$16.48',
+      discountedPrice: '$6.48',
+    },
+  ];
+
   return (
     <section className="flex flex-col items-center gap-8  ">
       <h3>BESTSELLER PRODUCTS</h3>
       <div className="lg:flex lg:flex-row lg:flex-wrap lg:justify-center gap-8">
-        <div className="">
-          <img src={catal} />
+        {/* Ürünlerin listesini haritalayalım */}
+        {products.map((product, index) => (
+          <div key={index} className="">
+            <img src={catal} alt={`Product ${index}`} />
 
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
+            <div className="flex flex-col items-start gap-4 w-[75%]">
+              <h5>{product.name}</h5>
+              <Link>{product.department}</Link>
+              <span className="flex flex-row gap-4">
+                <h5 className="text-softText">{product.price}</h5>
+                <h5 className="text-newP">{product.discountedPrice}</h5>
+              </span>
+            </div>
           </div>
-        </div>
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>{' '}
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>
-        <div className="">
-          <img src={catal} />
-
-          <div className="flex flex-col items-start gap-4 w-[75%]">
-            <h5>Graphic Design</h5>
-            <Link>English Department</Link>
-            <span className="flex flex-row gap-4">
-              <h5 className="text-softText">$16.48</h5>
-              <h5 className="text-newP">$6.48</h5>
-            </span>
-          </div>
-        </div>
+        ))}
       </div>
     </section>
   );
