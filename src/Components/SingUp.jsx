9f45ab7c1eb6d9a069b95dda68acda1c6cf38494
Atilla-Koz <atillakoz@hotmail.com>
2024-11-28 23:53:18 +0300

    T8: Sign Up User Form Page Fixes #18

--- src/Components/SingUp.jsx
+++ src/Components/SingUp.jsx
@@ -1,7 +1,23 @@
+import { useForm } from 'react-hook-form';
+
 export default function SignUp() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
   return (
-    <>
-      <h1>Sign Up</h1>
-    </>
+    <section className="flex flex-col items-center gap-8 pt-8">
+      <div className="hidden lg:block"></div>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="flex flex-col items-center gap-8"
+      >
+        <h3>Book Appointment</h3>
+        <label htmlFor="firstName">First Name</label>
+        <input {...register('firstName')} />
+
+        <input type="submit" />
+      </form>
+    </section>
   );
 }
